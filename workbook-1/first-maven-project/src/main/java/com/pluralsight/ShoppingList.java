3c0bbe9a916fcf1c829063da58500ae7910b5d33
Keante Young <youngkeante@gmail.com>
2024-09-26 10:05:16 -0400

    completed shoppinglist app

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println(" 1. Cereal 2. Eggs 3. Turkey 4. Toilet Paper 5. Toothpaste 6. Protein Shake 7. Chicken 8. Rice 9. Paper Towel 10. Cheese ");
+    }
+
+}
+
