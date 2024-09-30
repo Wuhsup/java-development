2315b356aedf89616e57e23090563e8993188a3c
Keante Young <youngkeante@gmail.com>
2024-09-30 13:33:37 -0400

    Add Intergers Calc

---
+++ workbook-1/financial-calculators/src/main/java/com/pluralsight/finanacialcalculator/mortgage.java
@@ -0,0 +1,12 @@
+package com.pluralsight.finanacialcalculator;
+
+public class mortgage {
+
+    public static void main(String[] args) {
+
+        double totalInterest = 0.0;
+        double principle = 0.0;
+        double interestRate = 0.0;
+        int_ loanLength = 0;
+    }
+}
